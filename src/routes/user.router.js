72e8ae672c1392c6974086efdeaaa6b22f96f6de
Nenubook <fernando.modesto.mendez@gmail.com>
2024-06-27 23:12:47 -0300

    prueba axios 2

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -6,7 +6,11 @@ export const userRouter = Router()
 userRouter.get('/', async (req, res) => {
     try {
         const response = await axios.get('http://menu-iota-ten.vercel.app/api/clientes/');
-        res.json(response.data);
+        if (response.data && response.data.status === 'success') {
+            res.json(response.data.payload);
+        } else {
+            res.status(500).json({ error: 'Unexpected response format' });
+        }
     } catch (error) {
         console.error('Error fetching data from external API:', error);
         res.status(500).json({ error: 'Failed to fetch data' });
