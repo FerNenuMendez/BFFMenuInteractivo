c23e46553694d226a60b93dc211cba7555d550ec
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-07-09 15:55:58 -0300

    cambios de user.service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -19,6 +19,7 @@ class UsuariosService {
             throw new Error('Failed to fetch data');
         }
     }
+
     async buscarUno(id) {
         try {
             const clientes = await axios.get('http://menu-iota-ten.vercel.app/api/clientes/');
@@ -33,33 +34,31 @@ class UsuariosService {
             throw new Error('Failed to fetch data');
         }
     }
+
     static async registrar(data) {
         try {
-            // Validar datos
-            if (!data.nombre || !data.apellido || !data.dni || !data.telefono || !data.mail || !data.password || !data.domicilio || !data.cuit) {
-                throw new Error('Todos los campos son obligatorios');
-            }
-            // Imprimir datos a enviar para verificar
-            console.info('Enviando datos:', JSON.stringify(data));
-            const response = await axios.post('http://menu-iota-ten.vercel.app/api/clientes/', data);
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+            const response = await axios.post('http://menu-iota-ten.vercel.app/api/clientes/', data, config);
             return response.data;
         } catch (error) {
             console.error('Error al registrar usuario:', error.message);
             if (error.response) {
-                // El servidor respondió con un código de estado que no está en el rango 2xx
                 console.error('Datos de la respuesta del error:', error.response.data);
                 console.error('Código de estado:', error.response.status);
                 console.error('Encabezados:', error.response.headers);
             } else if (error.request) {
-                // La solicitud fue hecha pero no se recibió respuesta
                 console.error('No se recibió respuesta:', error.request);
             } else {
-                // Algo pasó al configurar la solicitud que desencadenó un error
                 console.error('Error al configurar la solicitud:', error.message);
             }
             throw new Error('Failed to fetch data');
         }
     }
+
     async verificarCliente({ mail, password }) {
         try {
             // const user = await this.findOne({ mail }) buscar el usuario con buscarUno(id)
