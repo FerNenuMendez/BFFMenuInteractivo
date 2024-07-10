c0dcb4d57763228f1354a7366818bf414765011c
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-07-10 01:02:50 -0300

    cambios de data

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -7,9 +7,10 @@ export async function postController(req, res, next) {
     try {
         const data = req.body;
         const cliente = await usuariosService.verificarCliente(data);
-        logger.info(cliente);
+        logger.info(cliente.nombre, cliente.mail);
         req.user = cliente
-        res.status(200).json(cliente);
+        logger.info(req.user)
+        next()
     } catch (error) {
         logger.error(`Error message: ${error.message}`);
         logger.error(`Error stack: ${error.stack}`);
