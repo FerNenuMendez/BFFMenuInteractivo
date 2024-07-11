95b0ed0eeea87fbd5d1aa1cfb413dbbfe2c3ac0e
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-07-10 23:26:12 -0300

    cambios de controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,9 +5,11 @@ import { refreshToken } from '../middlewares/token.js';
 
 export async function getController(req, res, next) {
     try {
-        refreshToken(req, res)
-        logger.info(req.user)
-        return req.user
+        await refreshToken(req, res, async (err) => {
+            if (err) return next(err);
+            logger.info(req.user);
+            return res.status(200).send(req.user);
+        });
     } catch (error) {
         logger.error(`Error message: ${error.message}`);
         logger.error(`Error stack: ${error.stack}`);
