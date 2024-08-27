ecd9a79136fabfcd08673309cd1f7527573d077f
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-08-27 13:58:05 -0300

    cambios 2

--- src/services/pass.service.js
+++ src/services/pass.service.js
@@ -6,9 +6,13 @@ class PassService {
     async recuperarPassword(mail) {
         try {
             const response = await axios.post(`http://menu-iota-ten.vercel.app/api/password/forgot-password/`, { mail: mail });
+            logger.info(`Respuesta de la API externa: ${JSON.stringify(response.data, null, 2)}`);
             if (response.data && response.data.status === 'success') {
                 logger.info(response.data);
-                return response.data
+                return response.data;
+            } else if (response.data && response.data.error) {
+                logger.error(`Error devuelto por la API externa: ${response.data.error}`);
+                throw new Error(response.data.error);
             } else {
                 logger.error('Unexpected response format');
                 throw new Error('Unexpected response format');
