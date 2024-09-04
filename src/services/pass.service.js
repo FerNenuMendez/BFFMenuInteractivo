456f9e370ed24fcc06226aefcbd4ea7fd7f490d7
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-09-04 18:35:33 -0300

    cambios de errores 2

--- src/services/pass.service.js
+++ src/services/pass.service.js
@@ -6,7 +6,7 @@ class PassService {
     async recuperarPassword(mail) {
         try {
             const response = await axios.post('http://menu-iota-ten.vercel.app/api/password/forgot-password/', { mail });
-            logger.info(`Respuesta de la API externa: ${response.data}`); s
+            logger.info(`Respuesta de la API externa: ${response.data}`);
             if (response.status === 200 && response.data === 'Mail de recuperación de contraseña enviado') {
                 return { message: response.data };
             } else {
